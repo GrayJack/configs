04c2be2affaa9607595b5cc1422aa1127c4f1723
GrayJack <grayjack@localhost.localdomain>
2018-10-14 22:52:20 -0300

    Update my rc.lua

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -15,6 +15,7 @@ local hotkeys_popup = require("awful.hotkeys_popup").widget
                       require("awful.hotkeys_popup.keys")
 local my_table      = awful.util.table or gears.table -- 4.{0,1} compatibility
 local treetile      = require("treetile")
+treetile.focusnew = true
 -- }}}
 
 -- Configs
