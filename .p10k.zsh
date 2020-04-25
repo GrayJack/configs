b741b0544705e3e433e8f2ad7b6d56872c1f27af
GrayJack <gr41.j4ck@gmail.com>
2020-04-25 05:07:42 -0300

    Update p10k config

--- .p10k.zsh
+++ .p10k.zsh
@@ -128,7 +128,7 @@
   typeset -g POWERLEVEL9K_ICON_BEFORE_CONTENT=
 
   # Add an empty line before each prompt.
-  typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+  typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE=false
 
   # Connect left prompt lines with these symbols. You'll probably want to use the same color
   # as POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_FOREGROUND below.
